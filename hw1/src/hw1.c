16a1c4b0c800d006cc7bee21bac508c6b2887477
Haoyang Chen <Haoyang.chen@stonybrook.edu>
2017-02-02 15:36:05 -0500

    Checked num of args and -h

--- hw1/src/hw1.c
+++ hw1/src/hw1.c
@@ -1,12 +1,19 @@
 #include "hw1.h"
-
+#include <stdio.h>
 // For your helper functions (you may add additional files also)
 // DO NOT define a main function here!
 
 char validargs(int argc, char** argv, FILE** in, FILE** out) {
 	char ret = 0;
 
-	/* code here */
+	char* HELPFLAG = "-h";//, SUBFLAG = "-s", TUTFLAG = "-t";
+	if(argc < 2 || argc > 5){ //error if # of args < 1 or > 5
+		return ret;
+	}
 
+	char* arg1 = *(argv + 1);
+
+	if((*arg1 == *HELPFLAG) && (*arg1+1 == *HELPFLAG+1))
+		ret = ret | 0x80;
 	return ret;
 }
--- hw1/src/main.c
+++ hw1/src/main.c
@@ -5,8 +5,15 @@ int main(int argc, char **argv) {
     FILE* in;
     FILE* out;
 
+
     /* Note: create a variable to assign the result of validargs */
-    validargs(argc, argv, &in, &out);
+    char ret = validargs(argc, argv, &in, &out);
+
+    if(ret == 0)
+    	USAGE(EXIT_FAILURE);
+
+    if((ret & 0x80) == 0x80)
+    	USAGE(0);
 
     return EXIT_SUCCESS;
 }
