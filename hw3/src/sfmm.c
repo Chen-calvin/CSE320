df7238b9a9f3d98a18d8b686ac0fbf7fc39ef0ff
Haoyang Chen <Haoyang.chen@stonybrook.edu>
2017-03-10 17:45:11 -0500

    Free without coalesce working

--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -4,6 +4,7 @@
  * If you submit with a main function in this file, you will get a zero.
  */
 #include "sfmm.h"
+#include "hw3.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -155,7 +156,79 @@ void *sf_realloc(void *ptr, size_t size) {
 }
 
 void sf_free(void* ptr) {
-	return;
+	ptr = ((sf_free_header*)((char*)ptr - SF_HEADER_SIZE));
+	sf_free_header* header = (sf_free_header*)ptr;
+	(header)->header.alloc = 0;
+	sf_footer* footer = (sf_footer*)((char*)header + (header->header.block_size * 16) - SF_FOOTER_SIZE);
+	footer->alloc = 0;
+
+	sf_free_header* currBlock = freelist_head;
+	if(currBlock == NULL){
+		freelist_head = header;
+		return;
+	}
+
+	sf_free_header* spot = NULL;
+	while(currBlock != NULL){
+		if(header > currBlock)
+			spot = currBlock;
+		currBlock = currBlock->next;
+	}
+	if(spot == NULL){
+		freelist_head->prev = header;
+		header->next = freelist_head;
+		freelist_head = header;
+	}
+	else if(spot->next == NULL){
+		spot->next = header;
+		header->prev = spot;
+		}
+		else{
+			header->prev = spot;
+			header->next = spot->next;
+			spot->next = header;
+			spot->next->prev = header;
+		}
+	coalesce(header);
+}
+
+void* coalesce(sf_free_header* bp){
+	int prev_alloc = -1;
+	int next_alloc = -1;
+	size_t size = bp->header.block_size;
+
+	if(bp->prev != NULL)
+		prev_alloc = ((sf_footer*)((char*)bp - SF_HEADER_SIZE))->alloc;
+	if(bp->next != NULL)
+		next_alloc = ((sf_free_header*)((char*)bp + size))->header.alloc;
+
+	if((prev_alloc == 1 && next_alloc == 1) || (prev_alloc == -1 && next_alloc == -1))
+		return bp;
+	else if((prev_alloc == 1 && next_alloc == 0) || (prev_alloc == -1 && next_alloc == 0)){
+		size += bp->next->header.block_size;
+		bp->header.block_size = size;
+		((sf_footer*)((char*)bp->next + bp->next->header.block_size - SF_FOOTER_SIZE))->block_size = size;
+		bp->next = bp->next->next;
+		bp->next->next->prev = bp;
+	}
+		else if((prev_alloc == 0 && next_alloc == 1) || (prev_alloc == 0 && next_alloc == -1)){
+			size += bp->prev->header.block_size;
+			bp->prev->header.block_size = size;
+			((sf_footer*)((char*)bp + bp->header.block_size - SF_FOOTER_SIZE))->block_size = size;
+			bp->prev->next = bp->next;
+			bp->next->prev = bp->prev;
+			bp = bp->prev;
+		}
+		else{
+			size += bp->next->header.block_size + bp->prev->header.block_size;
+			bp->prev->header.block_size = size;
+			((sf_footer*)((char*)bp->next + bp->next->header.block_size - 8))->block_size = size;
+			bp->prev->next = bp->next->next;
+			bp->next->prev = bp->prev->prev;
+			bp = bp->prev;
+		}
+
+	return bp;
 }
 
 int sf_info(info* ptr) {
